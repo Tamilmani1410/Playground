10124882e4296bd61c4d5ac86fdd59e1af22a399
Tamilmani1410 <47779371+Tamilmani1410@users.noreply.github.com>
2019-04-20 13:32:23 +0530

    Test code pushed to repository.

---
+++ Armstrong Number /Main.java
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+   int n,sum=0,temp,rem,digit=0;
+  scanf("%d",&n);
+  temp=n;
+  while(temp!=0)
+  {
+    digit++;
+      temp=temp/10;
+  }
+  temp=n;
+  while(temp!=0)
+  {
+    rem=temp%10;
+      sum=sum+pow(rem,digit);
+    temp=temp/10;
+  }
+  if(n==sum)
+  printf("Armstrong Number");
+  else
+    printf("Not an Armstrong Number");
+  
+   return 0;
+}
---
+++ Sum of 'n' natural numbers using function/Main.java
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+  int a,count=0;
+    scanf("%d",&a);
+  for(int i=1;i<=a;i++)
+  {
+count=count+i;
+  }
+  printf("%d",count);
+    // Type your code here
+  	return 0;
+}
